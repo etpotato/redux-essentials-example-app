e4172d88b32e91d31200e8984600ea7bb4ca0910
etpotato <mareev.pv@gmail.com>
2022-03-29 16:04:48 +0300

    add post list and form

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+
+import { postAdded } from './postsSlice';
+
+const AddPostForm = () => {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
+  const handleTitleChange = (evt) => setTitle(evt.target.value);
+  const handleTextChange = (evt) => setText(evt.target.value);
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    const newPost = {
+      id: nanoid(),
+      title,
+      content: text,
+    };
+    if (title && text) dispatch(postAdded(newPost));
+
+    setTitle('');
+    setText('');
+  };
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={text}
+          onChange={handleTextChange}
+        />
+        <button type="submit">Save Post</button>
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ async function start() {
     <React.StrictMode>
       <Provider store={store}>
         <App />
+
       </Provider>
     </React.StrictMode>,
     document.getElementById('root')
